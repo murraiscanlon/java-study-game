32acbdfe4d4c121ac668cc069778fda288ef51ac
jjmacklin <jmacklin@seas.upenn.edu>
2020-04-10 14:44:38 -0400

    Updates scroll and text overlay

--- src/game/StoryPage.java
+++ src/game/StoryPage.java
@@ -2,6 +2,7 @@ package game;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.Icon;
@@ -12,6 +13,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -32,36 +36,59 @@ public class StoryPage extends JFrame {
         setContentPane(contentPane);
         
           
-        Icon icon2 = new ImageIcon("scroll_copy.png");
+        Icon icon2 = new ImageIcon("scrollpage1.png");       
         contentPane.setLayout(null);
         
-        JButton enter = new JButton("ENTER");
-        enter.setBackground(Color.LIGHT_GRAY);
+        ImageIcon buttonPic1 = new ImageIcon("enterButton.png");
+        JButton enter = new JButton("", buttonPic1);
+        enter.setPreferredSize(new Dimension(140, 40));
         enter.setOpaque(true);
         enter.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent arg0) {
                 SwingRoom room = new SwingRoom();//link to second page
-                room.setVisible(true); //brings up next screen
-                contentPane.setVisible(false);
+                room.setVisible(true); //brings up next screen                
                 
                 //need to figure out how to close current screen
             }
             
         });
-        enter.setFont(new Font("Castellar", Font.PLAIN, 27));
-        enter.setBounds(444, 340, 171, 41);
+        //enter.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        //enter.setBounds(578, 344, 140, 40);
         contentPane.add(enter);
-        JLabel image2 = new JLabel(icon2, JLabel.CENTER);
-        image2.setBounds(5, 27, 392, 480);
+        
+        JTextPane story = new JTextPane();
+        story.setEditable(false);
+        story.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+        story.setText("Before you lies a looming castle, "
+                        + "its moss-covered towers dark and damp. "
+                        + "A great enemy has overrun the castle and "
+                        + "captured Wizard Arvind, the most powerful "
+                        + "wizard in all the land. Your mission is "
+                        + "to defeat the lurking Java Monsters by"
+                        + "\r\nstealing their treasure, a feat which "
+                        + "can only be accomplished by correctly answering"
+                        + " questions about their language. If you collect "
+                        + "enough treasure, Wizard Arvind will be freed, and "
+                        + "the Java Monsters will be banished from the realm."
+                        + " Your Fairy GodTA will guide you on your journey. Good luck!");
+        story.setBounds(134, 36, 264, 365);
+        story.setOpaque(false);
+        story.setVisible(true);
+        contentPane.add(story);
+        JLabel image2 = new JLabel(icon2, SwingConstants.CENTER);
+        image2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        image2.setVerticalAlignment(SwingConstants.TOP);
+        image2.setBounds(65, 24, 400, 500);
         contentPane.add(image2);
         
         Icon bgImage = new ImageIcon("enterdoor.jpg");
+        
         JLabel background = new JLabel((Icon) bgImage, JLabel.CENTER);
         background.setBounds(0, 0, 784, 561);
         contentPane.add(background);
         
-
+        
     }
 } 
 
