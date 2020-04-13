138d5abd4fff75d45d63ded16b93ba39d033fd68
Murrai Scanlon <50279093+murraiscanlon@users.noreply.github.com>
2020-04-13 03:51:53 -0500

    Added documentation to QuestionDialogBox. Not done yet.

--- src/game/QuestionBoxDialog.java
+++ src/game/QuestionBoxDialog.java
@@ -28,37 +28,33 @@ import javax.swing.event.EventListenerList;
 public class QuestionBoxDialog extends JDialog implements ActionListener, PropertyChangeListener {
 
 	private static final long serialVersionUID = 1L;
+	private EventListenerList listenerList = new EventListenerList();
+	JLayeredPane layeredPane;
+	JPanel contentPane;
 	private JPanel questionPanel;
 	JLabel questLabel;
-	JRadioButton[] radioButtons;
-	ButtonGroup group;
-	ButtonGroup bg2;
-	JButton returnButton;
-	Treasure treasure;
+	JLabel currentQuestion;
+	JLabel treasureImageLabel;
 	JLabel treasureLabel;
-	Question question;
-	JPanel contentPane;
-	JLayeredPane layeredPane;
 	JLabel javaMonsterImageLabel;
+	JRadioButton[] radioButtons;
 	JRadioButton radioButton1;
 	JRadioButton radioButton2;
 	JRadioButton radioButton3;
 	JRadioButton radioButton4;
-	JLabel currentQuestion;
-	JLabel treasureImageLabel;
+	JButton returnButton;
+	JButton hintButton;
 	JButton submitButton;
-	boolean isCorrect;
+	ButtonGroup group;
+	ButtonGroup bg2;
+	Treasure treasure;
+	Question question;
 	Score s;
-
-	private EventListenerList listenerList = new EventListenerList();
-	
-
+	boolean isCorrect;
 
 	// Constructor
 	public QuestionBoxDialog(Frame aFrame) {
 		super(aFrame, true);// how does this work?
-		
-		
 
 		questLabel = new JLabel("Q");
 		radioButtons = new JRadioButton[4];
@@ -71,7 +67,7 @@ public class QuestionBoxDialog extends JDialog implements ActionListener, Proper
 		final String a3command = "a3";
 		final String a4command = "a4";
 
-		radioButtons[0] = new JRadioButton("A1");//How does this work? -Murrai
+		radioButtons[0] = new JRadioButton("A1");// How does this work? -Murrai
 		radioButtons[0].setActionCommand(a1command);
 		radioButtons[1] = new JRadioButton("A2");
 		radioButtons[1].setActionCommand(a2command);
@@ -93,9 +89,6 @@ public class QuestionBoxDialog extends JDialog implements ActionListener, Proper
 //			}
 //		});
 
-		
-		
-		
 		questionPanel = new JPanel();
 
 		questionPanel.add(questLabel);
@@ -108,40 +101,40 @@ public class QuestionBoxDialog extends JDialog implements ActionListener, Proper
 
 		// setContentPane(questionPanel);
 
-		/**********************************************
-		 * BASE FRAME WORK FOR DBQs
-		 *********************************************/
-
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 691, 762);
 
+		/**
+		 * The base of the QuestionBoxDialog window
+		 */
 		JPanel contentPane = new JPanel();
 		contentPane.setSize(500, 500);
 		contentPane.setBackground(Color.black);
 		contentPane.setBorder(new EmptyBorder(5, 100, 5, 5));
-
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 
+		/**
+		 * The layer that holds all of the labels and buttons
+		 */
 		JLayeredPane layeredPane = new JLayeredPane();
 		layeredPane.setSize(500, 500);
 		layeredPane.setBackground(Color.red);
 		layeredPane.setBounds(10, 10, 657, 700);
 		contentPane.add(layeredPane);
 
-		/*******************************************************
-		 * WINDOW COMPONENTS DBQs
-		 ************************************************/
-
+		/**
+		 * Creates current question position on layedPane background
+		 */
 		currentQuestion = new JLabel("Q");
 		currentQuestion.setOpaque(true);
 		currentQuestion.setBounds(35, 400, 573, 21);
 		layeredPane.add(currentQuestion);
 
-		/******************************************************
-		 * RADIO BUTTONS
-		 ********************************************/
-
+		/**
+		 * Creates answerChoice radio buttons and positions them on the layeredPane
+		 * background
+		 */
 		radioButton1 = new JRadioButton("R1");
 		radioButton1.setBounds(35, 440, 573, 21);
 		layeredPane.add(radioButton1);
@@ -158,41 +151,50 @@ public class QuestionBoxDialog extends JDialog implements ActionListener, Proper
 		radioButton4.setBounds(35, 530, 573, 21);
 		layeredPane.add(radioButton4);
 
+		/**
+		 * Groups the answerchoice buttons so that only one can be selected at a time.
+		 */
 		ButtonGroup bg2 = new ButtonGroup();
 		bg2.add(radioButton1);
 		bg2.add(radioButton2);
 		bg2.add(radioButton3);
 		bg2.add(radioButton4);
 
+		/**
+		 * SUBMIT button checks to see if answer choice is correct and adds treasure
+		 * points to score.
+		 */
 		submitButton = new JButton("Submit");// WORKING HERE
 		submitButton.setBounds(270, 584, 100, 21);
-		
+
 		/**
 		 * I need help working with this listener.
 		 */
 		submitButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-							
-				
-				 // only using this "if" to return a String to the fireQBEvent
+
+				// only using this "if" to return a String to the fireQBEvent
 				String checkAnswerString = "";
 				if (checkAnswer(question)) {
 					checkAnswerString = "TRUE";
-					//s.addPoints(1);//this should be treasure value
-					//System.out.println(s.getCurrentScore());//just testing
-					
+					// s.addPoints(1);//this should be treasure value
+					// System.out.println(s.getCurrentScore());//just testing
+
 				} else {
 					checkAnswerString = "FALSE";
 				}
 				bg2.clearSelection();
 				fireQBEvent(new QuestionBoxEvent(this, checkAnswerString));
-				
 
 			}
 		});
 		layeredPane.add(submitButton);
 
+		/**
+		 * Return button that closes the dialog box and returns the user to the last
+		 * visited room.
+		 */
 		returnButton = new JButton("Return");
 		returnButton.setBounds(475, 584, 100, 21);
 		returnButton.addMouseListener(new MouseAdapter() {
@@ -205,25 +207,39 @@ public class QuestionBoxDialog extends JDialog implements ActionListener, Proper
 		});
 		layeredPane.add(returnButton);
 
-
-
-		JLabel hintRevealedLabel = new JLabel("'" + /*displayHint(question) + */"'" + " - your FairyGodTA, Jami");//NOT WORKING CORRECTLY
+		/**
+		 * Holds the current hint for display once the hint button is clicked.
+		 */
+		JLabel hintRevealedLabel = new JLabel("'" + /* displayHint(question) + */"'" + " - your FairyGodTA, Jami");// not
+																													// working
 		hintRevealedLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		hintRevealedLabel.setBounds(103, 636, 391, 28);
 		layeredPane.add(hintRevealedLabel);
 		hintRevealedLabel.setVisible(false);
 
+		/**
+		 * Holds the image of the fairy in the frame.
+		 */
 		JLabel fairyRevealLabel = new JLabel("");
 		fairyRevealLabel.setIcon(new ImageIcon("fairy.png"));
 		fairyRevealLabel.setBounds(5, 547, 180, 163);
 		layeredPane.add(fairyRevealLabel);
 		fairyRevealLabel.setVisible(false);
-		
-		JButton hintButton = new JButton("HINT");//WORKING HERE
+
+		/**
+		 * Hint button displays the current hint, an image of the fairy, and subtracts 1
+		 * point for the players score
+		 */
+		JButton hintButton = new JButton("HINT");// WORKING HERE *************Not working
 		hintButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				//reduce score by one point
+//				if (s.currentScore > 0) {
+//					s.losePoints(1);
+//					System.out.println("current score " + s.getCurrentScore());// testing
+//				} else {
+//					// TODO game over?
+//				}
 				hintRevealedLabel.setVisible(true);
 				fairyRevealLabel.setVisible(true);
 				hintButton.setVisible(false);
@@ -232,23 +248,30 @@ public class QuestionBoxDialog extends JDialog implements ActionListener, Proper
 		});
 		hintButton.setBounds(70, 584, 100, 21);
 		layeredPane.add(hintButton);
-		
 
-
+		/**
+		 * Replaces the monster background image with a picture of the current treasure
+		 * collected.
+		 */
 		JLabel treasureImageLabel = new JLabel("");
 		treasureImageLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		treasureImageLabel.setIcon(new ImageIcon("diamond2.png"));
 		treasureImageLabel.setBounds(212, 50, 243, 180);
 		layeredPane.add(treasureImageLabel);
-		treasureImageLabel.setVisible(false);
+		treasureImageLabel.setVisible(true);
 
+		/**
+		 * Sends a message to the player that they have collected the current treasure.
+		 */
 		JLabel collectTreasureLabel = new JLabel("Message to Player");
 		collectTreasureLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		collectTreasureLabel.setBounds(212, 200, 267, 98);
 		layeredPane.add(collectTreasureLabel);
 		collectTreasureLabel.setVisible(false);
 
-		// LABEL FOR THE JAVA MONSTER BACKGROUND IMAGE
+		/**
+		 * Displays monster image as a background for the box frame
+		 */
 		JLabel javaMonsterImageLabel = new JLabel("");
 		javaMonsterImageLabel.setVerticalAlignment(SwingConstants.TOP);
 		javaMonsterImageLabel.setIcon(new ImageIcon(monsterGenerator()));
@@ -260,29 +283,26 @@ public class QuestionBoxDialog extends JDialog implements ActionListener, Proper
 
 	}// end constructor
 
-	/*****************************************************
-	 * METHODS
-	 *******************************************************/
-	
-	public String dummyMethod(String word) {
-		String x = word;
-		return x;
-	}
-	
-	public String displayHint(Question q) {//this is returning null??
+	/**
+	 * Helper method to display the current hint.
+	 */
+	public String displayHint(Question q) {// this is returning null??
 		question = q;
 		String currentHint = question.getHint();
 		return currentHint;
 	}
-	
 
+	/**
+	 * Compares the selected radioButton/answerchoice to the current
+	 * correctAnswer.
+	 */
 	public boolean checkAnswer(Question q) {
 		question = q;
 		int correctAnswerNumber = q.getCorrectAnswer();
 		isCorrect = false;
 		switch (correctAnswerNumber) {
 		case 1:
-			
+
 			return radioButton1.isSelected();
 		case 2:
 			return radioButton2.isSelected();
@@ -305,8 +325,6 @@ public class QuestionBoxDialog extends JDialog implements ActionListener, Proper
 		radioButton4.setText(q.getAnswer4());
 
 	}
-	
-
 
 	public void setUpTreasure(Treasure t) {
 		treasure = t;
@@ -314,6 +332,10 @@ public class QuestionBoxDialog extends JDialog implements ActionListener, Proper
 
 	}
 
+	/**
+	 * Generates a random monster picture for the background of the question dialog
+	 * box
+	 */
 	public String monsterGenerator() {
 		Random randomMonster = new Random();
 		ArrayList<String> javaMonsters = new ArrayList<String>();
