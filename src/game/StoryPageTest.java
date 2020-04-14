aa92533ab9be71edd63eb1559002ff89384c5888
Diane Hoffstetter <dianehof@seas.upenn.edu>
2020-04-14 12:38:17 -0700

    Trying to fix merge issue yet again

---
+++ src/game/StoryPageTest.java
@@ -0,0 +1,53 @@
+package game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+/**
+ * Method to test the StoryPage components
+ * @author mackl
+ *
+ */
+class StoryPageTest {
+
+    @Test
+    void testStoryPage() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSetFont() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testAddComponent() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testIsVisible() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testIsForegroundSet() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testIsBackgroundSet() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testGetBounds() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testGetMouseListeners() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ src/game/SwingEnterPageTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class to test Enter Page functionality
+ * @author Team 30
+ *
+ */
+class SwingEnterPageTest  {
+
+    /**
+     * Test method for SwingEnterPage.
+     */
+    @Test
+    void testSwingEnterPage() {
+        fail("Not yet implemented");
+    }
+
+}
