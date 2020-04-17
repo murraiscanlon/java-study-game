6b5c3f233065a5e1e7bf36004b30f3b2fc00ab50
Murrai Scanlon <50279093+murraiscanlon@users.noreply.github.com>
2020-04-17 13:33:40 -0500

    Question JTest all passing

--- src/game/QuestionTest.java
+++ src/game/QuestionTest.java
@@ -18,24 +18,24 @@ class QuestionTest {
 
 	@Test
 	void testQuestion() {
-		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "11", "14", "24", "PEMDAS", "answer3");
-		String expected = "What is the value of x? int x = (3 + 4) * 2" + "11" + "14" + "24" + "PEMDAS" + "answer3";
-		String actual = q.getQuestion()+ q.getAnswer1() + q.getAnswer2() + q.getAnswer3() + q.getHint() + q.getCorrectAnswer();
+		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "7", "11", "14", "24", "PEMDAS", 3);
+		String expected = "What is the value of x? int x = (3 + 4) * 27111424PEMDAS3";
+		String actual = q.getQuestion()+ q.getAnswer1() + q.getAnswer2() + q.getAnswer3() +  q.getAnswer4() + q.getHint() + q.getCorrectAnswer();
 		assertEquals(expected, actual);
 		
 	}
 
 	@Test
 	void testGetCorrectAnswer() {
-		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "11", "14", "24", "PEMDAS", "answer3");
-		String expected = "answer3";
-		String actual = q.getCorrectAnswer();
+		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "7", "11", "14", "24", "PEMDAS", 3);
+		int expected = 3;
+		int actual = q.getCorrectAnswer();
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	void testGetQuestion() {
-		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "11", "14", "24", "PEMDAS", "answer3");
+		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "7", "11", "14", "24", "PEMDAS", 3);
 		String expected = "What is the value of x? int x = (3 + 4) * 2";
 		String actual = q.getQuestion();
 		assertEquals(expected, actual);
@@ -44,15 +44,15 @@ class QuestionTest {
 
 	@Test
 	void testGetAnswer1() {
-		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "11", "14", "24", "PEMDAS", "answer3");
-		String expected = "11";
+		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "7", "11", "14", "24", "PEMDAS", 3);
+		String expected = "7";
 		String actual = q.getAnswer1();
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	void testSetAnswer1() {
-		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "testAnswer1", "14", "24", "PEMDAS", "answer3");
+		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "testAnswer1", "11", "14", "24", "PEMDAS", 3);
 		String expected = "testAnswer1";
 		String actual = q.getAnswer1();
 		assertEquals(expected, actual);
@@ -61,15 +61,15 @@ class QuestionTest {
 
 	@Test
 	void testGetAnswer2() {
-		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "11", "14", "24", "PEMDAS", "answer3");
-		String expected = "14";
+		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "7", "11", "14", "24", "PEMDAS", 3);
+		String expected = "11";
 		String actual = q.getAnswer2();
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	void testSetAnswer2() {
-		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "11", "testAnswer2", "24", "PEMDAS", "answer3");
+		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "7", "testAnswer2", "14", "24", "PEMDAS", 3);
 		String expected = "testAnswer2";
 		String actual = q.getAnswer2();
 		assertEquals(expected, actual);
@@ -77,15 +77,15 @@ class QuestionTest {
 
 	@Test
 	void testGetAnswer3() {
-		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "11", "14", "24", "PEMDAS", "answer3");
-		String expected = "24";
+		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "7", "11", "14", "24", "PEMDAS", 3);
+		String expected = "14";
 		String actual = q.getAnswer3();
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	void testSetAnswer3() {
-		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "11", "14", "testAnswer3", "PEMDAS", "answer3");
+		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "7", "11", "testAnswer3", "24", "PEMDAS", 3);
 		String expected = "testAnswer3";
 		String actual = q.getAnswer3();
 		assertEquals(expected, actual);
@@ -93,7 +93,7 @@ class QuestionTest {
 
 	@Test
 	void testGetHint() {
-		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "11", "14", "24", "PEMDAS", "answer3");
+		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "7", "11", "14", "24", "PEMDAS", 3);
 		String expected = "PEMDAS";
 		String actual = q.getHint();
 		assertEquals(expected, actual);
@@ -101,7 +101,7 @@ class QuestionTest {
 
 	@Test
 	void testSetHint() {
-		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "11", "14", "24", "testHint", "answer3");
+		Question q = new Question("What is the value of x? int x = (3 + 4) * 2", "7", "11", "14", "24", "testHint", 3);
 		String expected = "testHint";
 		String actual = q.getHint();
 		assertEquals(expected, actual);
