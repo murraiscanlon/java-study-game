7d06d8c84e1c11ddd809a034d85ad3aae44bdfaa
Diane Hoffstetter <dianehof@seas.upenn.edu>
2020-04-16 17:22:48 -0700

    Changed look of direction buttons

--- src/game/DirectionButton.java
+++ src/game/DirectionButton.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
 /**
  * Class to consolidate button parameters
  * @author Team 30
@@ -22,13 +24,23 @@ public class DirectionButton extends JButton {
 	public DirectionButton(String dir, int x, int y) {
 
 
+//		this.setText(dir);
+//		this.setIcon(null);
+//		this.setForeground(new Color(0, 0, 0));
+//		this.setBackground(new Color(128, 128, 128));
+//		this.setOpaque(true);
+//		this.setBounds(x, y, 80, 41);
+//		this.setEnabled(true);
+		
 		this.setText(dir);
-		this.setIcon(null);
-		this.setForeground(new Color(0, 0, 0));
-		this.setBackground(new Color(128, 128, 128));
-		this.setOpaque(true);
-		this.setBounds(x, y, 80, 41);
-		this.setEnabled(true);
+        this.setBorder(new LineBorder(new Color(153, 153, 153), 2));
+        this.setFont(new Font("Tahoma", Font.BOLD, 18));
+        this.setBackground(new Color(0, 60, 0));
+        this.setForeground(Color.WHITE);
+        this.setHorizontalAlignment(SwingConstants.CENTER);
+        this.setOpaque(true);
+        this.setBounds(x, y, 80, 41);
+		
 	
 	}
 		
