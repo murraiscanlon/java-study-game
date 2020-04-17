4a2dd6e878c4547d6957bc29002192472649bda7
jjmacklin <jmacklin@seas.upenn.edu>
2020-04-17 10:01:45 -0400

    Initial commit for Treasure image labels

---
+++ src/game/TreasureLabel.java
@@ -0,0 +1,33 @@
+package game;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+/**
+ * Class to consolidate treasure display
+ * Shows treasure icons when treasure is won
+ * @author Team 30 *
+ */
+public class TreasureLabel extends JLabel {
+    /**
+     * Instance Variables
+     */
+    private static final long serialVersionUID = 1L;
+    /**
+     * Constructor 
+     * @param x
+     * @param y
+     * @param image
+     */
+    public TreasureLabel(int x, int y, String image) {
+        this.setIcon(new ImageIcon(image));
+        this.setHorizontalAlignment(SwingConstants.CENTER);
+        this.setBackground(Color.GRAY);
+        this.setBounds(x, y, 80, 80);
+        this.setVisible(false);
+    }
+    
+
+}
