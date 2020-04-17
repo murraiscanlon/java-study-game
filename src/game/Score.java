4d346ed175713ea5924d3a5146b0e239de73ded3
jjmacklin <jmacklin@seas.upenn.edu>
2020-04-17 10:05:10 -0400

    Changed score target for testing

--- src/game/Score.java
+++ src/game/Score.java
@@ -15,7 +15,7 @@ public class Score {
 	private ArrayList<Treasure> treasures = new ArrayList<Treasure>();
 	private int currentScore = 0;
 	private int numberOfQuestionsWrong = 0;
-	final int scoreTarget = 5;
+	final int scoreTarget = 10;
 	final int wronqQuestionTarget = 20;
 	private String name = "Bob";
     
