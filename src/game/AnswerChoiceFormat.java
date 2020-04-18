e4a989b96e0c27bda7d87f0d3890a6de2b6fe89a
Murrai Scanlon <50279093+murraiscanlon@users.noreply.github.com>
2020-04-18 16:09:18 -0500

    Added comments to QuestionAnswerFormat class

--- src/game/AnswerChoiceFormat.java
+++ src/game/AnswerChoiceFormat.java
@@ -5,10 +5,19 @@ import java.awt.Font;
 
 import javax.swing.JRadioButton;
 
+/**
+ * This class extends JRadioButton to create formatting for answer choice buttons in the QuestionBoxDialog class.
+ * @author Team 30
+ *
+ */
 public class AnswerChoiceFormat extends JRadioButton {
 	
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * This method improves readability in questionBoxDialog class by storing code that is used multiple places.
+	 * @param y - The y coordinate for the answer choice radio button
+	 */
 	public AnswerChoiceFormat(int y) {
 		
 		this.setBounds(35, y, 275, 40);
